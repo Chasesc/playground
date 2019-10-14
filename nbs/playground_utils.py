f2a103dd5f9ef217d73154b892886e06e294c09c
Chase Schelthoff <schelthoff.chase@gmail.com>
2019-10-13 22:22:22 -0400

    load data

--- nbs/playground_utils.py
+++ nbs/playground_utils.py
@@ -1,10 +1,36 @@
 '''
 misc utils
 '''
-
+from pathlib import Path
 from types import ModuleType
+from tqdm import tqdm
 
+import collections
 import sys
+import os
+
+
+IMAGENET_MEAN = [0.485, 0.456, 0.406]
+IMAGENET_STD  = [0.229, 0.224, 0.225]
+IMAGENET_NORMALIZATION = dict(mean=IMAGENET_MEAN, std=IMAGENET_STD)
+
+def get_dataset_distribution(ds, classes=None, progress=True):
+    if hasattr(ds, 'targets'): # if we already have targets, just use those
+        targets = ds.targets
+    else:
+        indices = range(len(ds))
+        if progress:
+            indices = tqdm(indices)
+        targets = (ds[idx][-1] for idx in indices)
+
+    if classes:
+       targets = (classes[target] for target in targets)
+    
+    return collections.Counter(targets)
+
+def print_and_return(x, file=sys.stdout):
+    print(x, file=file)
+    return x
 
 def module_version(module, file=sys.stdout):
     version = hasattr(module, '__version__') and module.__version__
